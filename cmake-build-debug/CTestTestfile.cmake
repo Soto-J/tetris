f95fd1c9221fed6861c9c5e6c4f4da8a386e0646
Mr Smithy x <charltonsmith@outlook.com>
2024-10-31 17:58:15 -0400

    tetris obj can stop at bottom

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/charl/CLionProjects/tetris
+# Build directory: C:/Users/charl/CLionProjects/tetris/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/fmt-build")
+subdirs("_deps/raylib-build")
