0ea5a98517a777196d209d3ce0bb75ea6cc50fc6
Mr Smithy x <charltonsmith@outlook.com>
2024-10-31 23:31:16 -0400

    rm

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/charl/CLionProjects/tetris
-# Build directory: C:/Users/charl/CLionProjects/tetris/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/fmt-build")
-subdirs("_deps/raylib-build")
