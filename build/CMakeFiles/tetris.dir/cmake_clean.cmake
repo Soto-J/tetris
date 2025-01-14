61319a4a08f6142a2325a1302ae2839b578ecf3e
john soto <johnsoto@johns-MacBook-Air.local>
2025-01-14 15:57:37 -0500

    Update

---
+++ build/CMakeFiles/tetris.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetris.dir/game/Grid.cpp.o"
+  "CMakeFiles/tetris.dir/game/Grid.cpp.o.d"
+  "CMakeFiles/tetris.dir/game/Tetris.cpp.o"
+  "CMakeFiles/tetris.dir/game/Tetris.cpp.o.d"
+  "CMakeFiles/tetris.dir/main.cpp.o"
+  "CMakeFiles/tetris.dir/main.cpp.o.d"
+  "tetris"
+  "tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
